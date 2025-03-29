66a149b7ebcf6b3b5ab933197499b4bdc4c8b3d6
Valeria <05valarist@gmail.com>
2025-03-29 11:58:01 -0500

    Creación del proyecto spring boot

---
+++ sping-boot-docker/src/main/java/com/example/sping_boot_docker/SpingBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sping_boot_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootDockerApplication.class, args);
+	}
+
+}
---
+++ sping-boot-docker/src/test/java/com/example/sping_boot_docker/SpingBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sping_boot_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
