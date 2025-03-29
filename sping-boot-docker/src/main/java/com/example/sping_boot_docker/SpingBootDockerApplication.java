142f390d93d6f6d3f78da93f396f9060472dbccb
Valeria <05valarist@gmail.com>
2025-03-29 12:46:15 -0500

    Dockerfile y compilación

--- sping-boot-docker/src/main/java/com/example/sping_boot_docker/SpingBootDockerApplication.java
+++ sping-boot-docker/src/main/java/com/example/sping_boot_docker/SpingBootDockerApplication.java
@@ -2,10 +2,21 @@ package com.example.sping_boot_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Imports para poder construir el aplicativo web
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class SpingBootDockerApplication {
 
+	//en el main de la web se verá el mensaje que retornamos
+	@RequestMapping("/")
+	public String home() {
+		return "Hola mundo desde spring boot con docker";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpingBootDockerApplication.class, args);
 	}
